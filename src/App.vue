39c2e657aacb39d25ca5008d2b75787939922f4d
Adetola Adewodu <adetola.adewodu@gmail.com>
2020-03-24 07:46:28 -0400

    Committing initial geo-vue project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <geomap></geomap>
+  </div>
+</template>
+
+<script>
+import geomap from "./components/geomap";
+
+export default {
+  name: "App",
+  components: {
+    geomap
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/geoinput.vue
@@ -0,0 +1,25 @@
+ <template>
+  <p align="left">
+    Latitude (N/S):
+    <br>
+    <input type="number" step="0.01" name="Latitude">
+    <br>Longitude (E/W):
+    <br>
+    <input type="number" step="0.01" name="Longitude">
+    <br>Radius (miles):
+    <br>
+    <input type="number" step="0.01" name="RadiusMiles">
+    <br>
+    <button onclick="createCircleFromCoordinates()">Create Circle From Coordinates</button>
+    <br>
+    <button onclick="removeCircle()">Remove Circles</button>
+    <br>City, State:
+    <input type="text" name="address">
+    <br>Radius (miles):
+    <input type="number" step="0.01" name="addressRadius">
+    <!--button onclick="createCircleFromAddress()">Create Polygon From Address</button>
+    
+    <button onclick="findCoordinates()">Add Point</button>
+    <button onclick="removePoints()">Remove all Points</button-->
+  </p>
+</template>
